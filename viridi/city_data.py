350e21d1cf70b6b56be2c721abdb5229bdc7e69b
steinagel <williansteinagel@gmail.com>
2020-05-30 22:25:52 -0300

    wip
    Signed-off-by: steinagel <williansteinagel@gmail.com>

--- viridi/city_data.py
+++ viridi/city_data.py
@@ -3,6 +3,7 @@ from flask_restful import Resource
 
 from viridi.data_models.mocked_data.ultra_data_science import MockedData
 
+# from serpapi.google_search_results import GoogleSearchResults
 
 class CityData(Resource):
     @staticmethod
@@ -19,3 +20,32 @@ class CityData(Resource):
         }
 
         return res, 200
+
+    # def get(self):
+    #     print(request.form['city'])
+    #     month = 4
+    #     from_day = 2
+    #     to_day = 3
+    #     year = 2020
+
+    #     params = {
+    #         "engine": "google",
+    #         "q": "Trump",
+    #         "google_domain": "google.com",
+    #         "tbm": "nws",
+    #         "tbs": f"cdr:1,cd_min:{month}/{from_day}/{year},cd_max:{month}/{to_day}/{year}",
+    #     }
+
+    #     client = GoogleSearchResults(params)
+    #     data = client.get_dict()
+
+    #     print("News results")
+
+    #     for result in data['news_results']:
+    #         print(f"""
+    #     Title: {result['title']}
+    #     Snippet: {result['snippet']}
+    #     Date: {result['date']}
+    #     """)    
+
+    #     return None, 200
